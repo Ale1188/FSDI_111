dbad9fed8de7b62655412c7300ef1fb170bc26df
Ale1188 <dramoizta@gmail.com>
2024-08-06 14:45:15 -0700

    Class 4 - 111

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,50 @@
-from flask import Flask
+from flask import(
+    Flask,
+    request
+)
+from app.database import task
 
 app = Flask(__name__)
 
-@app.get("/")
+#http get method
 
-def index():
-    me = {
-        "first_name": "Alejandro",
-        "first_name": "Barragan",
-        "hobbies": "Watch movies and series",
-        "is_online": True
+@app.get("/tasks")
+def get_all_tasks():
+    task_list = task.scan()
+    out = {
+        "tasks" : task_list,
+        "ok" : True
     }
-    return me
+    return out
+
+@app.get("/tasks/<int:pk>/")
+def get_single_task(pk):
+    single_task = task.select_by_id(pk)
+    if single_task:
+        out = {
+            "task": single_task,
+            "ok": True
+        }
+        return out
+    out = {
+        "ok": False,
+        "message":"Not Found"
+    }
+    return out, 404
+
+@app.post("/tasks")
+def create_task():
+    task_data = request.json
+    task.insert(task_data)
+    return "", 204
+
+@app.put("/tasks/<int:pk>/")
+def update_task(pk):
+    task_data = request.json
+    task.update_by_id(task_data,pk)
+    return "", 204
+
+@app.delete("/tasks/<int:pk>/")
+def delete_task(pk):
+    task.delet_by_id(pk)
+    return "", 204
